5641db41e444f9aef0f2f81b9047f96610d59a85
lzif <lukyfriendly@gmail.com>
2025-11-06 05:17:35 +0000

    feat: add brat

--- src/commands/sticker.ts
+++ src/commands/sticker.ts
@@ -6,27 +6,20 @@ module.exports = {
 	name: "sticker",
 	aliases: ["s"],
 	code: async (ctx: Ctx) => {
-		if (
-			ctx.getMessageType() === MessageType.imageMessage ||
-			ctx.getMessageType() === MessageType.videoMessage
-		) {
-			const buffer = ((await ctx.msg.media.toBuffer()) ||
-				(await ctx.quoted.media.toBuffer())) as Buffer<ArrayBufferLike>
-			if (!buffer) {
-				ctx.reply(italic("❌ Reply ke media atau jadikan sebagai caption."))
-				return
-			}
-			const sticker = new Sticker(buffer, {
-				pack: "Mambo",
-				author: "El Mambo",
-				type: StickerTypes.FULL,
-				categories: [],
-				id: "12345",
-				quality: 100,
-			})
-			ctx.reply({ sticker: await sticker.toBuffer() })
-		} else {
-			ctx.reply("Please reply to an image or video to create a sticker.")
+		const buffer = ((await ctx.msg.media.toBuffer()) ||
+			(await ctx.quoted.media.toBuffer())) as Buffer<ArrayBufferLike>
+		if (!buffer) {
+			ctx.reply(italic("❌ Reply ke media atau jadikan sebagai caption."))
+			return
 		}
+		const sticker = new Sticker(buffer, {
+			pack: "Mambo",
+			author: "El Mambo",
+			type: StickerTypes.FULL,
+			categories: [],
+			id: "12345",
+			quality: 100,
+		})
+		ctx.reply(await sticker.toMessage())
 	},
 } as Command
